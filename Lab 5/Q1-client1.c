413731908275a95e27b39be405dbbaff2f8ef56c
Richa Shanbhag <149705539+richashanbhag@users.noreply.github.com>
2026-02-13 05:26:11 +0530

    Create Q1-client1.c

---
+++ Lab 5/Q1-client1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+
+int main()
+{
+    int sockfd;
+    struct sockaddr_in servaddr;
+    char msg[] = "Institute Of";
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+    send(sockfd, msg, strlen(msg) + 1, 0);
+
+    close(sockfd);
+    return 0;
+}
